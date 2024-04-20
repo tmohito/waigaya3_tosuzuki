3dd77e821820db6815c9c58b6d1ee4ca1ef8f9a1
mahara <rmahara@pacific-systems.co.jp>
2024-04-20 17:47:26 +0900

    HandsOnフォルダ作成

---
+++ Chapter3_課題解説/HandsOn_DB接続リファクタリング/Controllers/Request/Category/SaveRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Waigaya3.Controllers.Request.Category
+{
+    public class SaveRequest
+    {
+        public int Id { get; set; }
+
+        public required string Title { get; set; }
+    }
+}
---
+++ Chapter3_課題解説/HandsOn_DB接続リファクタリング/Controllers/Request/Product/SaveRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace Waigaya3.Controllers.Request.Product
+{
+    public class SaveRequest
+    {
+        public int? Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public string? Description { get; set; } = "";
+
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
